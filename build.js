a912ce90477a15024e3907859bafde8c2c46202e
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-11-06 21:13:57 +0900

    chore(workspace): prepare workspace

---
+++ build.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import { join } from 'path';
+import { cwd } from 'process';
+import { build } from 'esbuild';
+import { nodeExternalsPlugin } from 'esbuild-node-externals';
+
+build({
+    entryPoints: [join(cwd(), 'src', 'index.ts')],
+    outdir: join(cwd(), 'out'),
+    bundle: true,
+    minify: true,
+    sourcesContent: false,
+    allowOverwrite: true,
+    format: 'esm',
+    platform: 'node',
+    sourcemap: 'inline',
+    tsconfig: join(cwd(), 'tsconfig.json'),
+    plugins: [nodeExternalsPlugin()],
+});
