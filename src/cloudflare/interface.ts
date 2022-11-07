3b883ac625f0b88081b8a6154bee834fabc3b5c9
eve0415 <40632436+eve0415@users.noreply.github.com>
2022-11-07 12:29:59 +0900

    chore: add code

---
+++ src/cloudflare/interface.ts
@@ -0,0 +1,33 @@
+export interface CFZoneResponse {
+    result: {
+        id: string;
+        name: string;
+        status: 'active';
+        paused: boolean;
+        type: 'full';
+        development_mode: number;
+        name_servers: string[];
+        original_name_servers: string[];
+        original_registrar: string;
+        original_dnshost: string | null;
+        modified_on: string;
+        created_on: string;
+        activated_on: string;
+        meta: {
+            step: number;
+            custom_certificate_quota: number;
+            page_rule_quota: number;
+            phishing_detected: boolean;
+            multiple_railguns_allowed: boolean;
+        };
+        owner: {
+            id: string;
+            type: string;
+            email: string;
+        };
+        account: {
+            id: string;
+            name: string;
+        };
+    };
+}
