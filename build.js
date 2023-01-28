a6f2306fd4febfe866af068097a69339aeed1243
eve0415 <40632436+eve0415@users.noreply.github.com>
2023-01-28 13:10:53 +0900

    build: update build script

--- build.js
+++ build.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
-import { join } from 'path';
-import { cwd } from 'process';
-import { build } from 'esbuild';
+// @ts-check
 
-build({
+import { error, log } from 'console';
+import { context } from 'esbuild';
+import { join } from 'path';
+import prettyBytes from 'pretty-bytes';
+import { argv, cwd, exit } from 'process';
+
+const ctx = await context({
   entryPoints: [join(cwd(), 'src', 'index.ts')],
   outdir: join(cwd(), 'out'),
   bundle: true,
@@ -16,4 +20,32 @@ build({
   sourcemap: 'inline',
   tsconfig: join(cwd(), 'tsconfig.json'),
   packages: 'external',
+  metafile: true,
+  plugins: [
+    {
+      name: 'bundle-size',
+      setup: ({ onEnd }) => {
+        onEnd(({ errors, metafile }) => {
+          if (errors.length) {
+            error(errors);
+            return;
+          }
+
+          const size = Object.values(metafile?.outputs ?? {}).reduce((a, b) => a + b.bytes, 0);
+          log(`Build complete with ${prettyBytes(size, { locale: 'ja' })}`);
+        });
+      },
+    },
+  ],
 });
+
+process.on('beforeExit', () => {
+  ctx.dispose().catch(error);
+});
+
+if (!argv.includes('--watch')) {
+  await ctx.rebuild();
+  exit();
+}
+
+await ctx.watch();
